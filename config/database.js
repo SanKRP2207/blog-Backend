e660dc9311dcaf8355efcbec23d297202c084fc3
SanKRP2207 <sandeepkumarpal281@gmail.com>
2024-08-06 19:13:18 +0530

    Db 3306

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_PORT, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
   port: process.env.DB_PORT || 3306,
