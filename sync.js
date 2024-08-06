c5fdc7802b54ca72681ac9c2d46e177a0657ec79
SanKRP2207 <sandeepkumarpal281@gmail.com>
2024-08-06 12:43:30 +0530

    error fixs

--- sync.js
+++ sync.js
@@ -1,4 +1,3 @@
-// sync.js
 const sequelize = require('./config/database');
 const User = require('./models/User');
 const Post = require('./models/Post');
@@ -7,9 +6,12 @@ const Like = require('./models/Like');
 
 const syncDb = async () => {
   try {
+    console.log('Attempting to connect to the database...');
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    await sequelize.sync({ force: true }); // Use { force: true } to drop tables and re-create them
+
+    console.log('Synchronizing models...');
+    await sequelize.sync({ force: true });
     console.log('All models were synchronized successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
