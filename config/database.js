5a6332d48a4a1947a77fd32c6f29d12d20cb502b
SanKRP2207 <sandeepkumarpal281@gmail.com>
2024-08-06 19:25:49 +0530

    3306

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_PORT, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
   port: process.env.DB_PORT || 3306,
+  dialect: 'mysql',
 });
 
 module.exports = sequelize;
